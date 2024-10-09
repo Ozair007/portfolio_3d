ac3dc3fd96644776b0a24b12689b05ef29fd6ca4
Ozair Aziz <uzair.aziz34@gmail.com>
2024-10-09 23:55:57 +0300

    Added multiple sections including contact and footer sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import About from "./sections/About";
+import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
+// import Clients from "./sections/Clients";
 import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
+import Projects from "./sections/Projects";
 
 const App = () => {
   return (
     <main className="max-w-7xl mx-auto">
       <Navbar />
       <Hero />
+      <About />
+      <Projects />
+      {/* <Clients /> */}
+      <Contact />
+      <Footer />
     </main>
   );
 };
